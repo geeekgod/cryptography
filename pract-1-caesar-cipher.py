c47f4747ba133362afd6c8dce04874b6d1f76206
Rishabh Singh <56874272+thisisrishabh22@users.noreply.github.com>
2023-01-19 14:03:22 +0530

    feat: add encrypt code for caesar cipher

--- pract-1-caesar-cipher.py
+++ pract-1-caesar-cipher.py
@@ -1,1 +1,19 @@
+def encrypt(text,s):
+    result = ""
+   # transverse the plain text
+    for i in range(len(text)):
+        char = text[i]
+        # Encrypt uppercase characters in plain text
+        if (char.isupper()):
+            result += chr((ord(char) + s-65) % 26 + 65)
+        # Encrypt lowercase characters in plain text
+        else:
+            result += chr((ord(char) + s - 97) % 26 + 97)
+    return result
 
+text = input("Enter the text: ")
+s = 4
+
+print("Plain Text : " + text)
+print("Shift pattern : " + str(s))
+print("Cipher: " + encrypt(text,s))
